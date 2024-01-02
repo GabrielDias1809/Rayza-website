bcb2a39578b61cf423f6c31c3407bcdd83f18bfc
Gabriel <diasbusiness18@gmail.com>
2024-01-02 18:12:22 -0300

    ajustando feed

--- src/components/InstaFeed.jsx
+++ src/components/InstaFeed.jsx
@@ -7,7 +7,7 @@ const InstaFeed = () => {
         src="https://static.elfsight.com/platform/platform.js"
         data-use-service-core
         defer
-      />
+      ></script>
       <div
         className="elfsight-app-2805d832-0dc9-4dfe-acda-20dc28f6149b"
         data-elfsight-app-lazy
